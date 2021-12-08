ec04b8746874ecdc624393dfa90f2644cfbcb4e4
Drews <amartinovich.01@gmail.com>
2021-12-08 17:06:03 +0300

    Described Vue errorHandler/warnHandler

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,21 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+let errsLogging;
+let warnsLogging;
+
+app.config.errorHandler = (err, vm, info) => {
+  console.log('APP ERROR:\nError Trace:', err, '\nComponent: ', vm, '\nInfo: ', info);
+  const errInfo = { error: err, component: vm, information: info };
+  errsLogging.push(errInfo);
+}
+
+app.config.warnHandler = (msg, vm, trace) => {
+  console.log('APP WARN:\nMessage: ', msg, '\nComponent: ', vm, '\nTrace: ', trace);
+  const warnInfo = { message: msg, component: vm, hierarchy: trace };
+  warnsLogging.push(warnInfo);
+}
+
+app.use(router).mount('#app');
